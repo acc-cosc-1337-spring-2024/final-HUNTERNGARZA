bb9b8a9235ebfcd537e8548450028b0e7bb49fff
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-05-04 17:46:52 +0000

    solved question d

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,12 @@
-#add import
+import question_d
+
+
+while True:
+    print("run till u say stop")
+
+    table = question_d.create_multiplication_table()
+    question_d.display_multiplication_table(table)
+
+    choice = input("continue? ")
+    if choice == "no":
+        break
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,23 @@
 #write functions here, don't add input('') statements here!
+
+
+def create_multiplication_table():
+    table = []
+
+    for i in range(1, 6):
+        for j in range(1, 11):
+            table.append(i*j)
+
+    return table
+
+
+def display_multiplication_table(table):
+
+    count = 10
+
+    for i in table:
+        if i % count == 0:
+            print(i)
+            count += 10
+        else:
+            print(i, end=" ")

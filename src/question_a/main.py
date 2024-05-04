461854e61e2c5a13d9e6800d9dde70ad39af9050
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-05-04 17:03:46 +0000

    Changed the program for question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,5 +1,10 @@
-import question_a
 
+series_of_numbers = []
 
-question_a.program_series_of_5_numbers()
-# it didnt say to add tests so i didnt
+for i in range(5):
+    series_of_numbers.append(int(input("Enter a number: ")))
+        
+print(f"Lowest Number: {min(series_of_numbers)}")
+print(f"Highest Number: {max(series_of_numbers)}")
+print(f"Total Numbers: {sum(series_of_numbers)}")
+print(f"Average of Numbers: {sum(series_of_numbers)/len(series_of_numbers)}")
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -2,14 +2,3 @@
 def test_config():
     return True
 
-
-def program_series_of_5_numbers():
-    series_of_numbers = []
-
-    for i in range(5):
-        series_of_numbers.append(int(input("Enter a number: ")))
-        
-    print(f"Lowest Number: {min(series_of_numbers)}")
-    print(f"Highest Number: {max(series_of_numbers)}")
-    print(f"Total Numbers: {sum(series_of_numbers)}")
-    print(f"Average of Numbers: {sum(series_of_numbers)/len(series_of_numbers)}")

ded53646505ce66d2f04f378ca3db9118fae48f2
HUNTERNGARZA <157082334+HUNTERNGARZA@users.noreply.github.com>
2024-05-04 17:13:29 +0000

    Solved question b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,12 @@
-#add import
+import question_b
+
+
+while True:
+    print("your menu")
+    print(" 1-Display stock purchase history")
+    print("2-Exit")
+    user_choice = input("Choice? ")
+    if user_choice == "1":
+        question_b.stock_purchase_history()
+    else:
+        break
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,3 +1,35 @@
 #write functions here, don't add input('') statements here!
 
+class Stock:
 
+    def __init__(self, symbol, company_name) -> None:
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+    
+    def get_company_name(self):
+        return self.__company_name
+    
+
+def stock_purchase_history():
+    appl_stock = Stock("APPL", "Apple Inc")
+    cat_stock = Stock("CAT", "Caterpillar")
+    ek_stock = Stock("EK", "Eastman Kodak")
+    goog_stock = Stock("GOOG", "Google")
+    msft_stock = Stock("MSFT", "Microsoft")
+
+    stock_dictionary = {
+        "Apple Stock": appl_stock,
+        "Caterpillar Stock": cat_stock,
+        "Eastman Kodak Stock": ek_stock,
+        "Google Stock": goog_stock,
+        "Microsoft Stock": msft_stock
+    }
+
+    print("Stock list")
+    print("Symbol | Company Name")
+    for i in stock_dictionary:
+        stock = stock_dictionary[i]
+        print(f"{stock.get_symbol()} | {stock.get_company_name()}")
